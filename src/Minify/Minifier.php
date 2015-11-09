78ab8bb7b33adb4c72c2c154a3792b3167c5d616
Eric Walker <eewalk11@gmail.com>
2015-11-09 11:24:35 -0700

    modified Minifier to store a URI after it is generated

--- src/Minify/Minifier.php
+++ src/Minify/Minifier.php
@@ -39,6 +39,14 @@ class Minifier implements JsonSerializable
 
 
 
+	/**
+	 * @var string|boolean Once a URI is generated for this Minifier it will be stored here. If any
+	 * settings are changed it will be reset to null.
+	 */
+	protected $uri = null;
+
+
+
 	/*
 	 * CONSTRUCTOR / DESTRUCTOR
 	 */
@@ -102,13 +110,11 @@ class Minifier implements JsonSerializable
 	 * <p>If the file has already been added, this method will do nothing.</p>
 	 * @param string $file The path of the file to add relative to the public directory, or to the
 	 * Minify base directory if set.</p>.
+	 * @return boolean True if the file was added, false otherwise.
 	 */
 	public function addFile($file)
 	{
-		if($file && !in_array($file, $this->files))
-		{
-			$this->files[] = $file;
-		}
+		return $this->addItem($file, $this->files);
 	}
 
 
@@ -120,18 +126,11 @@ class Minifier implements JsonSerializable
 	 * is the same as calling addFile(). A falsey value will be treated as an empty array. The path
 	 * of each file should be relative to the public directory, or to the Minify base directory if
 	 * set.
+	 * @return array An array of each new file added.
 	 */
 	public function addFiles($files)
 	{
-		if(!is_array($files))
-		{
-			$files = $files ? [$files] : [];
-		}
-
-		foreach($files as $file)
-		{
-			$this->addFile($file);
-		}
+		$this->addArray($files, $this->files);
 	}
 
 
@@ -141,13 +140,11 @@ class Minifier implements JsonSerializable
 	 * <p>The group must be a group defined in the Minify groupsConfig.php file. If the group has
 	 * already been added, this method will do nothing.</p>
 	 * @param string $group The Minify group.
+	 * @return boolean True if the group was added, false otherwise.
 	 */
 	public function addGroup($group)
 	{
-		if($group && !in_array($group, $this->groups))
-		{
-			$this->groups[] = $group;
-		}
+		return $this->addItem($group, $this->groups);
 	}
 
 
@@ -158,24 +155,20 @@ class Minifier implements JsonSerializable
 	 * already been added, this method will ignore them.</p>
 	 * @param mixed $groups An aray of Minify groups. A single group can be passed as well, which is
 	 * the same as calling addGroup(). A falsey value will be treated as an empty array.
+	 * @return array An array of each new group added.
 	 */
 	public function addGroups($groups)
 	{
-		if(!is_array($groups))
-		{
-			$groups = $groups ? [$groups] : [];
-		}
-
-		foreach($groups as $group)
-		{
-			$this->addGroup($group);
-		}
+		$this->addArray($groups, $this->groups);
 	}
 
 
 
 	/**
 	 * Create a URI to minify the added files.
+	 * <p>Once the URI has been generated it will be stored in this Minifier until any files,
+	 * groups, or the base is changed. This allows the URI to be retrieved multiple times with only
+	 * being generated once.</p>
 	 * <p>A MinifyException will be thrown if debugging is enabled and this Minifier contains
 	 * invalid settings.</p>
 	 * @return string|boolean The minify URI, false if nothing was added to minify.
@@ -183,8 +176,14 @@ class Minifier implements JsonSerializable
 	 */
 	public function createUri()
 	{
-		$builder = new UriBuilder($this);
-		return $builder->buildUri();
+		if($this->uri === null)
+		{
+			//Generate the URI
+			$builder = new UriBuilder($this);
+			$this->uri = $builder->buildUri();
+		}
+
+		return $this->uri;
 	}
 
 
@@ -230,13 +229,7 @@ class Minifier implements JsonSerializable
 	 */
 	public function removeFile($file)
 	{
-		$rem = false;
-		if(($index = array_search($file, $this->files)) !== false)
-		{
-			unset($this->files[$index]);
-			$rem = true;
-		}
-		return $rem;
+		return $this->removeItem($file, $this->files);
 	}
 
 
@@ -250,20 +243,7 @@ class Minifier implements JsonSerializable
 	 */
 	public function removeFiles($files)
 	{
-		if(!is_array($files))
-		{
-			$files = $files ? [$files] : [];
-		}
-
-		$rem = [];
-		foreach($files as $file)
-		{
-			if($this->removeFile($file))
-			{
-				$rem[] = $file;
-			}
-		}
-		return $rem;
+		return $this->removeArray($files, $this->files);
 	}
 
 
@@ -275,39 +255,20 @@ class Minifier implements JsonSerializable
 	 */
 	public function removeGroup($group)
 	{
-		$rem = false;
-		if(($index = array_search($group, $this->groups)) !== false)
-		{
-			unset($this->groups[$index]);
-			$rem = true;
-		}
-		return $rem;
+		$this->removeItem($group, $this->groups);
 	}
 
 
 
 	/**
 	 * Remove an array of Minify groups from this Minifier.
-	 * @param mixed $groups An aray of Minify groups. A single group can be passed as well, which is
-	 * the same as calling removeGroup(). A falsey value will be treated as an empty array.
+	 * @param array|string $groups An aray of Minify groups. A single group can be passed as well,
+	 * which is the same as calling removeGroup(). A falsey value will be treated as an empty array.
 	 * @return array An array containing all group names that were removed.
 	 */
 	public function removeGroups($groups)
 	{
-		if(!is_array($groups))
-		{
-			$groups = $groups ? [$groups] : [];
-		}
-
-		$rem = [];
-		foreach($groups as $group)
-		{
-			if($this->removeGroup($group))
-			{
-				$rem[] = $group;
-			}
-		}
-		return $rem;
+		return $this->removeArray($groups, $this->groups);
 	}
 
 
@@ -319,7 +280,164 @@ class Minifier implements JsonSerializable
 	 */
 	public function setBase($dir)
 	{
-		$this->base = $dir || is_string($dir) ? $dir : false;
+		$newBase = $dir || is_string($dir) ? $dir : false;
+
+		if($newBase != $this->base)
+		{
+			$this->clearUri();
+		}
+
+		$this->base = $newBase;
+	}
+
+
+
+	/*
+	 * PROTECTED METHODS
+	 */
+
+
+
+	/**
+	 * Clear this Minifier's stored URI.
+	 */
+	protected function clearUri()
+	{
+		$this->uri = null;
+	}
+
+
+
+	/*
+	 * PRIVATE METHODS
+	 */
+
+
+
+	/**
+	 * Add an array of items to one of this Minifier's data arrays.
+	 * @param array|string $add The items to add.
+	 * @param array $array Reference to the array to add to.
+	 * @return array An array containing all new items that were added.
+	 */
+	private function addArray($add, &$array)
+	{
+		//Convert the argument to an array
+
+		if(!is_array($add))
+		{
+			$add = $add ? [$add] : [];
+		}
+
+		//Add all items in the array
+
+		$added = [];
+
+		foreach($added as $item)
+		{
+			if($this->addItem($item, $array))
+			{
+				$added[] = $item;
+			}
+		}
+
+		//NOTE: Do not clear the stored URI here, calling $this->addItem() will clear it
+
+		return $added;
+	}
+
+
+
+	/**
+	 * Add an item to one of this Minifier's data arrays.
+	 * @param string $item The item to add.
+	 * @param array $array Reference to the array to add to.
+	 * @return boolean True if the item was added, false otherwise.
+	 */
+	private function addItem($item, &$array)
+	{
+		//Add the item
+
+		$added = false;
+
+		if($item && is_string($item) && !in_array($item, $array))
+		{
+			$array[] = $item;
+			$added = true;
+		}
+
+		//Clear the stored URI if the item was added
+
+		if($added)
+		{
+			$this->clearUri();
+		}
+
+		return $added;
+	}
+
+
+
+	/**
+	 * Remove an array of items from one of this Minifier's data arrays.
+	 * @param array|string $rem The items to remove.
+	 * @param array $array Reference to the array to remove from.
+	 * @return array An array containing all items that were removed.
+	 */
+	private function removeArray($rem, &$array)
+	{
+		//Convert the argument to an array
+
+		if(!is_array($rem))
+		{
+			$rem = $rem ? [$rem] : [];
+		}
+
+		//Remove all items in the array
+
+		$removed = [];
+
+		foreach($rem as $item)
+		{
+			if($this->removeItem($item, $array))
+			{
+				$removed[] = $item;
+			}
+		}
+
+		//NOTE: Do not clear the stored URI here, calling $this->removeItem() will clear it
+
+		return $removed;
+	}
+
+
+
+	/**
+	 * Remove an item from one of this Minifier's data arrays.
+	 * @param sttring $item The item to remove.
+	 * @param array $array Reference to the array to remove from.
+	 * @return boolean True if the item was removed from the array, false if the item was not found.
+	 */
+	private function removeItem($item, &$array)
+	{
+		//Remove the item from the array
+
+		$rem = false;
+
+		if(($index = array_search($item, $array)) !== false)
+		{
+			unset($array[$index]);
+			$rem = true;
+		}
+
+		//Clear the stored URI if an item was removed
+
+		if($rem)
+		{
+			$this->clearUri();
+		}
+
+		return $rem;
 	}
 
 
